b14118fcb5a22c3071f38a9ba9a72cc5766c343c
ncc <nc@me.com>
2022-01-29 18:53:30 -0500

    minor clean up

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from pprint import pprint
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def main():
@@ -30,8 +26,5 @@ def main():
     pprint(matches)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
