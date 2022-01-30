1503410d355de8aad83e701b45b2d4483fab576f
ncc <nc@me.com>
2022-01-29 21:28:11 -0500

    minor clean up

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 from pprint import pprint
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def main():
-    words_file_path = r'/home/nitesh/PycharmProjects/spelling_bee/words.txt'
-    words_list = []
+@app.get("/buzz/")
+def create_list_of_candidate_words(key_letter: str, supporting_letters: str):
+    words_file_path = r'./words.txt'
+    matches = []
+    all_letters = list(supporting_letters) + [key_letter]
     with open(words_file_path, mode='rt', encoding='utf-8') as f:
         for line in f:
-            words_list.append(line.strip().lower())
-
-    key_letter = 'h'
-    supporting_letters = ['n', 'g', 'z', 'e', 'w', 'i'] + [key_letter]
-
-    matches = []
-    for word in words_list:
-        if key_letter in word:
-            matched = True
-            for c in word:
-                if c not in supporting_letters:
-                    matched = False
-                    break
-            if matched:
+            word = line.strip().lower()
+            if len(word) <= 3:
+                continue
+            if key_letter in word and set(word).issubset(all_letters):
                 matches.append(word)
     matches.sort(key=lambda x: len(x), reverse=True)
 
-    pprint(matches)
-
-
-if __name__ == '__main__':
-    main()
+    return matches
