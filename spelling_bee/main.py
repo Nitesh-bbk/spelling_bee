aee6ba12f7a6760fb61ee9d4bb7e2cf582223556
ncc <nc@me.com>
2022-01-29 21:51:27 -0500

    re-format source code.

---
+++ spelling_bee/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+words_file_path = None
+
+
+@app.get("/buzz/")
+def generate_word_list(key_letter: str, supporting_letters: str):
+    global words_file_path
+    if words_file_path is None:
+        words_file_path = r'data/words.txt'
+
+    key_letter = key_letter.lower()
+    supporting_letters = supporting_letters.lower()
+
+    matches = []
+    all_letters = list(supporting_letters) + [key_letter]
+    with open(words_file_path, mode='rt', encoding='utf-8') as f:
+        for line in f:
+            word = line.strip().lower()
+            if len(word) <= 3:
+                continue
+            if key_letter in word and set(word).issubset(all_letters):
+                matches.append(word)
+    matches.sort(key=lambda x: len(x), reverse=True)
+
+    return matches
