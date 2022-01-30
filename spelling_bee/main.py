ca27b1fe21a17eb91390e60e300cea5dddb1c5d3
Nitesh C <nch@chd.com>
2022-01-30 11:23:02 -0500

    add dockerfile and make container-ready

--- spelling_bee/main.py
+++ spelling_bee/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 words_file_path = None
@@ -6,10 +7,10 @@ words_file_path = None
 
 @app.get("/buzz/")
 def generate_word_list(key_letter: str, supporting_letters: str):
+    print(os.curdir)
     global words_file_path
     if words_file_path is None:
         words_file_path = r'./spelling_bee/data/words.txt'
-
     key_letter = key_letter.lower()
     supporting_letters = supporting_letters.lower()
 
--- tests/generate_word_list_tests.py
+++ tests/generate_word_list_tests.py
@@ -1,16 +1,16 @@
-import spelling_bee.main
+from spelling_bee import main
 from spelling_bee.main import generate_word_list
 import unittest
 
 
 class GenerateWordListTests(unittest.TestCase):
     def test_should_generate_word_list(self):
-        spelling_bee.main.words_file_path = r'../tests/data/test_words.txt'
+        main.words_file_path = r'../tests/data/test_words.txt'
         results = generate_word_list(key_letter='c', supporting_letters='oxtnei')
         self.assertEqual(17, len(results))
 
     def test_should_generate_word_list_case_insensitive(self):
-        spelling_bee.main.words_file_path = r'../tests/data/test_words.txt'
+        main.words_file_path = r'../tests/data/test_words.txt'
         results = generate_word_list(key_letter='c', supporting_letters='oxtneI')
         self.assertEqual(17, len(results))
 
